dca14240f5cbe1e0c9dd1fa2c744c97a8a186316
Junior Alves <jr290488@gmail.com>
2015-02-20 14:23:19 -0200

    teste search

--- index.php
+++ index.php
@@ -1,39 +1,83 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
-
-require './vendor/autoload.php';
-
-use Facebook\FacebookSession;
-use Facebook\FacebookRedirectLoginHelper;
-use Facebook\FacebookRequest;
-use Facebook\GraphUser;
-use Facebook\FacebookRequestException;
-
-// $facebook = new Facebook(array(
-    // 'appId' => '846603932071076',
-    // 'secret' => '213db9ce119a812e54b39d72c49ce7ec',
-// ));
-
-
-FacebookSession::setDefaultApplication('846603932071076', '213db9ce119a812e54b39d72c49ce7ec');
-
-// $helper = new FacebookRedirectLoginHelper('https://www.facebook.com/blogsaudediaadia');
-// $loginUrl = $helper->getLoginUrl();
-// Use the login url on a link or button to redirect to Facebook for authentication
-
-$helper = new FacebookRedirectLoginHelper();
-try {
-  $session = $helper->getSessionFromRedirect();
-} catch(FacebookRequestException $ex) {
-  // When Facebook returns an error
-} catch(\Exception $ex) {
-  // When validation fails or other local issues
-}
-if ($session) {
-  // Logged in
-}
-
-print_r($helper);
-
+//
+// require './vendor/autoload.php';
+//
+// use Facebook\FacebookSession;
+// use Facebook\FacebookRequest;
+// use Facebook\GraphUser;
+// use Facebook\FacebookRequestException;
+//
+// FacebookSession::setDefaultApplication('1517330805208053', 'c20b912bebdf0527aadaa24183415f32');
+//
+// if($session) {
+//
+// try {
+//
+// $user_profile = (new FacebookRequest(
+// $session, 'GET', '/me'
+// ))->execute()->getGraphObject(GraphUser::className());
+//
+// echo "Name: " . $user_profile->getName();
+//
+// } catch(FacebookRequestException $e) {
+//
+// echo "Exception occured, code: " . $e->getCode();
+// echo " with message: " . $e->getMessage();
+//
+// }
+//
+// }
+//
+// print_r($helper);
 ?>
+
+<html>
+	<head>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+		<title>jQuery Example</title>
+		<script>
+			$(document).ready(function() {
+
+				$.ajaxSetup({
+					cache : true
+				});
+
+				$.getScript('//connect.facebook.net/en_UK/all.js', function() {
+					FB.init({
+						appId : '935188619828681',
+						cookie : true, // enable cookies to allow the server to access
+						// the session
+						xfbml : true, // parse social plugins on this page
+						version : 'v2.1' // use version 2.1
+					});
+					//$('#loginbutton,#feedbutton').removeAttr('disabled');
+
+					FB.getLoginStatus(function(response) {
+						console.log(response);
+
+						if (response.status === 'connected') {
+							console.log('Welcome!  Fetching your information.... ');
+							// FB.api('/me', function(response) {
+								// console.log('Successful login for: ' + response.name);
+							// });
+							
+							var tok = FB.getAccessToken();
+							FB.api('/search', {
+								'access_token' : tok,
+								"type" : "adkeyword",
+								"q" : "marketing"
+							}, function(response) {
+								console.log(response);
+							});
+						}
+					});
+				});
+			});
+		</script>
+	</head>
+	<body>
+		<fb:login-button scope="public_profile,email" onlogin="checkLoginState();"></fb:login-button>
+	</body>
+</html>
